7dc354d3dca7f30780980aa1f3d03dbf00712888
Chris Armstrong <carmstrong1gw@github.com>
2021-04-10 00:12:51 -0400

    Several commits, but working with API keys.

--- bco_api/api/apps.py
+++ bco_api/api/apps.py
@@ -1,5 +1,18 @@
+# Run code after start-up
+# TODO: move things from settings.py into here.
+# Source: https://stackoverflow.com/a/42744626/5029459
+# Source: https://docs.djangoproject.com/en/3.2/ref/applications/#django.apps.AppConfig.ready
+
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class ApiConfig(AppConfig):
+    
     name = 'api'
+
+    def ready(self):
+        
+        # Create the anonymous user if they don't exist.
+        from .signals import populate_models
+        post_migrate.connect(populate_models, sender=self)
--- bco_api/api/models.py
+++ bco_api/api/models.py
@@ -1,18 +1,28 @@
-from django.db import models
-
 # Explanation of optional fields:  https://stackoverflow.com/questions/16349545/optional-fields-in-django-models
 # TextField is used here because it has no character limit.
 
 # Create a base model, then inherit for each table.
 # See the 4th example under "Model Inheritance" at https://docs.djangoproject.com/en/3.1/topics/db/models/#model-inheritance
 
+from django.db import models
+
 from django.conf import settings
+
 # For reading the configuration file.
 from api.scripts import DbUtils
 
 # --- Permissions imports --- #
 
-from django.contrib.auth.models import User
+# Linking API keys to users.
+# Source: https://florimondmanca.github.io/djangorestframework-api-key/guide/#api-key-models
+from rest_framework_api_key.models import AbstractAPIKey
+
+# Source: https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+
+# The user model is straight from Django.
+from django.contrib.auth.models import Group, User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Generic JSON model
@@ -70,28 +80,12 @@ class meta_table(models.Model):
 		abstract = True
 
 
-# Generic API key model
-class api_keys(models.Model):
-
-
-	# The number of objects in a given table.
-
-	# Field is required.
-	key = models.CharField(max_length = 1000)
-
-	# Each key is tied to certain groups.
-	groups = models.JSONField()
-
-
-	# Make this class a parent.
-	class Meta:
-		abstract = True
-
-
 # Link prefixes to groups
 class prefix_groups(models.Model):
 
 	# Each prefix has exactly one group owner.
+
+	# The choices of group are taken directly from the table.
 
 	prefix = models.CharField(max_length = 5)
 
@@ -99,14 +93,14 @@ class prefix_groups(models.Model):
 
 
 # Link prefixes to tables
-class prefix_tables(models.Model):
+# class prefix_tables(models.Model):
 
-	# Each prefix can only be tied to one table,
-	# but multiple prefixes can be tied to one table.
+# 	# Each prefix can only be tied to one table,
+# 	# but multiple prefixes can be tied to one table.
 
-	table = models.CharField(max_length = 1000)
+# 	table = models.CharField(max_length = 1000)
 
-	prefixes = models.JSONField()
+# 	prefixes = models.JSONField()
 
 
 # TODO: put all of this under a flag later so that cloning
@@ -116,7 +110,7 @@ class prefix_tables(models.Model):
 models_dict = {}
 
 # Read the configuration file.
-db_settings_from_file = DbUtils.DbUtils().load_settings_file(file_path='./tables.conf')
+db_settings_from_file = DbUtils.DbUtils().load_settings_file(file_path = './tables.conf')
 
 # Go through each template and create the associated table.
 for template, tables in db_settings_from_file.items():
@@ -144,7 +138,51 @@ settings.MODELS = models_dict
 # --- Permissions models --- #
 
 
-# API Information is kept separate so that we can use it
+# User and API Information is kept separate so that we can use it
 # elsewhere easily.
 
-# ... To be put in ...
+# Source: https://florimondmanca.github.io/djangorestframework-api-key/guide/#api-key-models
+# Source: https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+
+
+
+
+# If we want a separate API users table.
+# # All users for the API.
+# class api_users(models.Model):
+	
+# 	user = models.OneToOneField(User, on_delete = models.CASCADE)
+# 	group = models.ForeignKey(Group, on_delete = models.CASCADE)
+
+# 	# Display the username properly in the admin.
+# 	# Source: https://stackoverflow.com/a/61991068/5029459	
+# 	def __str__(self):
+# 		return self.user.username
+
+# Link user creation to groups.
+@receiver(post_save, sender=User)
+def associate_user_group(sender, instance, created, **kwargs):
+	
+	if created:
+		
+		# Create a group for this user.
+		# Source: https://stackoverflow.com/a/55206382/5029459
+		Group.objects.create(name = instance)
+		group = Group.objects.get(name = instance)
+		group.user_set.add(instance)
+
+
+# If we want a separate API users table.
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+# 	instance.User.save()
+
+
+# Link API keys to users
+class api_users_api_key(AbstractAPIKey):
+	
+	user = models.ForeignKey(
+		User,
+		on_delete = models.CASCADE,
+		related_name = 'api_keys'
+	)
