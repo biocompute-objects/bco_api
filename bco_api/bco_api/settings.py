3538f9a08104c0602cc11baf5a4af4799a0e787c
Hadley King <HadleyKing@users.noreply.github.com>
2021-11-19 15:34:04 -0500

    Update settings.py
    Add proper `/static` root and location

--- bco_api/bco_api/settings.py
+++ bco_api/bco_api/settings.py
@@ -216,11 +216,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-   ]
+STATIC_URL = '/api/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/var/www/bcoeditor/bco_api/bco_api/static/'
 
 # ----- CUSTOM VARIABLES AND METHODS ----- #
 
