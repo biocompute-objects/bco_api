84be17b4b27cd00acd8f1ead1571302f65bdd06f
John Torcivia <syntheticgio@aol.com>
2021-12-05 13:43:10 -0500

    Support for being able to fetch published BCOs in the format http://localhost:8000/BCO_000010/1.0 (for example)

--- bco_api/api/scripts/method_specific/GET_published_object_by_id_with_version.py
+++ bco_api/api/scripts/method_specific/GET_published_object_by_id_with_version.py
@@ -24,37 +24,40 @@ def GET_published_object_by_id_with_version(oi_root, oi_version):
     if underscores < 1:
         # ERROR - there should be an underscore separating the prefix and the bco name
         return Response(
-            data='This API requires that the prefix and the BCO name be separated by an underscore \'_\' in the object_id_root PATH variable.',
-            status=status.HTTP_400_BAD_REQUEST
-        )
+                data='This API requires that the prefix and the BCO name be separated by an underscore \'_\' in the object_id_root PATH variable.',
+                status=status.HTTP_400_BAD_REQUEST
+                )
 
     # TODO: This allows BCO Names to support underscores - not sure if that is valid though
     #       This can be 'fixed' by adding in a check for > 1 above
     #       Might be a better idea to split prefix, bco name, and version into a three part get
     bco_prefix, bco_name = oi_root.split("_", maxsplit=1)
 
-    retrieved = list(
-        bco.objects.filter(
-            # contents__search=bco_name,
-            prefix=bco_prefix,
-            contents__provenance_domain__name=bco_name,
-            contents__provenance_domain__version=oi_version,
-            state='PUBLISHED'
-        ).values_list(
-            'contents',
-            flat=True
-        )
-    )
-    # # The object ID either exists or it does not.
+    import pdb;
+    pdb.set_trace()
+
     # retrieved = list(
-    #     bco.objects.filter(
-    #         object_id__regex = rf'(.*?)/{oi_root}/{oi_version}',
-    #         state = 'PUBLISHED'
-    #     ).values_list(
-    #         'contents',
-    #         flat = True
-    #     )
-    # )
+    #         bco.objects.filter(
+    #                 # contents__search=bco_name,
+    #                 prefix=bco_prefix,
+    #                 contents__provenance_domain__name=bco_name,
+    #                 contents__provenance_domain__version=oi_version,
+    #                 state='PUBLISHED'
+    #                 ).values_list(
+    #                 'contents',
+    #                 flat=True
+    #                 )
+    #         )
+    # The object ID either exists or it does not.
+    retrieved = list(
+            bco.objects.filter(
+                    object_id__regex=rf'(.*?)/{oi_root}/{oi_version}',
+                    state='PUBLISHED'
+                    ).values_list(
+                    'contents',
+                    flat=True
+                    )
+            )
     # Was the object found?
     if len(retrieved) > 0:
         # Kick it back.
@@ -64,9 +67,9 @@ def GET_published_object_by_id_with_version(oi_root, oi_version):
         # the root ID does not exist at all.
         print('No objects were found for the root ID and version provided.')
         return Response(
-            data='No objects were found for the root ID and version provided.',
-            status=status.HTTP_400_BAD_REQUEST
-        )
+                data='No objects were found for the root ID and version provided.',
+                status=status.HTTP_400_BAD_REQUEST
+                )
 
     # TODO: This code from here on down appears to be unreachable?  The above if/else will always return the request
     #       Maybe this is placeholder code for something?
@@ -91,38 +94,38 @@ def GET_published_object_by_id_with_version(oi_root, oi_version):
         if apps.get_model(
                 app_label='api',
                 model_name=table_name
-        ).objects.filter(
-            object_id=constructed
-        ).exists():
+                ).objects.filter(
+                object_id=constructed
+                ).exists():
 
             # Get the object, then check the permissions.
             objected = apps.get_model(
-                app_label='api',
-                model_name=table_name
-            ).objects.get(
-                object_id=constructed
-            )
+                    app_label='api',
+                    model_name=table_name
+                    ).objects.get(
+                    object_id=constructed
+                    )
 
             return Response(
-                data=serializers.serialize(
-                    'json',
-                    [objected, ]
-                ),
-                status=status.HTTP_200_OK
-            )
+                    data=serializers.serialize(
+                            'json',
+                            [objected, ]
+                            ),
+                    status=status.HTTP_200_OK
+                    )
 
         else:
 
             return (
-                Response(
-                    status=status.HTTP_400_BAD_REQUEST
-                )
+                    Response(
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
             )
 
     else:
 
         return (
-            Response(
-                status=status.HTTP_400_BAD_REQUEST
-            )
+                Response(
+                        status=status.HTTP_400_BAD_REQUEST
+                        )
         )
