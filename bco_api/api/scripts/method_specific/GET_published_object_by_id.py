2900ad2bfad0a34b20bc72f66365cbb088fff8b2
John Torcivia <syntheticgio@aol.com>
2021-12-05 15:52:56 -0500

    allow for optional patch version, updated the latest version search

--- bco_api/api/scripts/method_specific/GET_published_object_by_id.py
+++ bco_api/api/scripts/method_specific/GET_published_object_by_id.py
@@ -5,6 +5,53 @@ from ...models import bco
 from rest_framework import status
 from rest_framework.response import Response
 
+# Below is helper code to deal with how we are allowing non standard versions (i.e. 1.2 instead of 1.2.0, etc).
+import re
+from semver import VersionInfo as Version
+from typing import Optional, Tuple
+
+
+BASEVERSION = re.compile(
+    r"""[vV]?
+        (?P<major>0|[1-9]\d*)
+        (\.
+        (?P<minor>0|[1-9]\d*)
+        (\.
+            (?P<patch>0|[1-9]\d*)
+        )?
+        )?
+    """,
+    re.VERBOSE,
+)
+
+
+def coerce(version: str) -> Tuple[Version, Optional[str]]:
+    """
+    Convert an incomplete version string into a semver-compatible Version
+    object
+
+    * Tries to detect a "basic" version string (``major.minor.patch``).
+    * If not enough components can be found, missing components are
+        set to zero to obtain a valid semver version.
+
+    :param str version: the version string to convert
+    :return: a tuple with a :class:`Version` instance (or ``None``
+        if it's not a version) and the rest of the string which doesn't
+        belong to a basic version.
+    :rtype: tuple(:class:`Version` | None, str)
+    """
+    match = BASEVERSION.search(version)
+    if not match:
+        return (None, version)
+
+    ver = {
+        key: 0 if value is None else value for key, value in match.groupdict().items()
+    }
+    ver = Version(**ver)
+    rest = match.string[match.end() :]  # noqa:E203
+    return ver, rest
+
+
 def GET_published_object_by_id(oi_root):
     """
     Get a published object given a root.
@@ -22,52 +69,42 @@ def GET_published_object_by_id(oi_root):
     slash).
 
     """
-    oi_root = oi_root.split("_")[0] +  '{:06d}'.format(int(oi_root.split("_")[1]))
+
+    # Note: This is not needed - removing out the underscore breaks the regex below, leaving in for the moment
+    #       since I'm not sure why it was ever added (maybe there is a reason?)
+    # oi_root = oi_root.split("_")[0] +  '{:06d}'.format(int(oi_root.split("_")[1]))
     all_versions = list(
-        bco.objects.filter(
-            object_id__regex = rf'(.*?)/{oi_root}/',
-            state = 'PUBLISHED'
-        ).values_list(
-            'object_id',
-            flat = True
-        )
-    )
+            bco.objects.filter(
+                    object_id__regex=rf'(.*?)/{oi_root}/',
+                    state='PUBLISHED'
+                    ).values_list(
+                    'object_id',
+                    flat=True
+                    )
+            )
 
     # Get the latest version for this object if we have any.
     if len(all_versions) > 0:
-        
+
         # There was at least one version of the root ID,
         # so now perform some logic based on whether or
         # not a version was also passed.
-        
+
         # First find the latest version of the object.
-        latest_major = 0
-        latest_minor = 0
+        latest_version = [i.split('/')[-1:][0] for i in all_versions]
+        l_version, _ = coerce(max(latest_version, key=coerce))
 
-        latest_version = [
-            i.split('/')[-1:][0] for i in all_versions
-        ]
-
-        for i in latest_version:
-            
-            major_minor_split = i.split('.')
-            
-            if int(major_minor_split[0]) >= latest_major:
-                if int(major_minor_split[1]) >= latest_minor:
-                    latest_major = int(major_minor_split[0])
-                    latest_minor = int(major_minor_split[1])
-        
         # Kick back the latest version.
         return Response(
-            data = bco.objects.filter(
-                object_id__regex = rf'{oi_root}/{latest_major}.{latest_minor}',
-                state = 'PUBLISHED'
-            ).values_list(
-                'contents',
-                flat = True
-            ),
-            status = status.HTTP_200_OK
-        )
+                data=bco.objects.filter(
+                        object_id__regex=rf'{oi_root}/{l_version.major}.{l_version.minor}?.?{l_version.patch}',
+                        state='PUBLISHED'
+                        ).values_list(
+                        'contents',
+                        flat=True
+                        ),
+                status=status.HTTP_200_OK
+                )
 
     else:
 
@@ -75,6 +112,6 @@ def GET_published_object_by_id(oi_root):
         # the root ID does not exist at all.
         print('No objects were found for the root ID provided.')
         return Response(
-            data = 'No objects were found for the root ID provided.',
-            status = status.HTTP_400_BAD_REQUEST
-        )
+                data='No objects were found for the root ID provided.',
+                status=status.HTTP_400_BAD_REQUEST
+                )
