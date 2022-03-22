2d5569c0319059cb9110395e06feca5c1ce9c388
Hadley King <hadley_king@gwu.edu>
2022-03-22 15:04:18 -0400

    Fix autofield warning messages Changes to be committed: 	modified:   api/admin.py 	modified:   api/apps.py 	modified:   api/models.py 	modified:   api/signals.py 	modified:   bco_api/settings.py

--- bco_api/api/admin.py
+++ bco_api/api/admin.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Django Admin
 
 Registers models for the Django Admin app
@@ -6,8 +7,10 @@ Registers models for the Django Admin app
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 from api.models import bco, prefix_table, new_users, Prefix
+from api.groups import GroupInfo
 
 admin.site.register(bco, GuardedModelAdmin)
 admin.site.register(prefix_table)
 admin.site.register(new_users)
+admin.site.register(GroupInfo)
 admin.site.register(Prefix)
--- bco_api/api/apps.py
+++ bco_api/api/apps.py
@@ -1,24 +1,21 @@
-# Run code after start-up
-# TODO: move things from settings.py into here.
-# Source: https://stackoverflow.com/a/42744626/5029459
-# Source: https://docs.djangoproject.com/en/3.2/ref/applications/#django.apps.AppConfig.ready
+#!/usr/bin/env python3
+"""Run code after start-up
+TODO: move things from settings.py into here.
+Source: https://stackoverflow.com/a/42744626/5029459
+Source: https://docs.djangoproject.com/en/3.2/ref/applications/#django.apps.AppConfig.ready
+"""
 
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from api.signals import populate_models
 
+class ApiConfig(AppConfig):
+    """API Configuration
+    """
 
-class ApiConfig(
-        AppConfig
-        ):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'api'
 
-    def ready(
-            self
-            ):
-        # Create the anonymous user if they don't exist.
-        from .signals import populate_models
-
-        post_migrate.connect(
-                populate_models,
-                sender=self
-                )
+    def ready(self):
+        """Create the anonymous user if they don't exist."""
+        post_migrate.connect(populate_models, sender=self)
