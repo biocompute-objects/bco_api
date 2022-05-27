8ac655cac089bc2ec82549a4a5acb41a4e4c02e4
John Torcivia <syntheticgio@aol.com>
2022-05-27 10:05:32 -0400

    fix so doesn't pass as string (pass as dict)

--- bco_api/api/tests/test_prefix_post_api.py
+++ bco_api/api/tests/test_prefix_post_api.py
@@ -5,11 +5,19 @@
 """
 
 import json
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import Group, Permission, User
+from rest_framework.authtoken.models import Token
+
 # from django.urls import reverse
 from api.models import BCO
+from api.views import ApiObjectsDraftsCreate
+from api.scripts.method_specific.POST_api_objects_drafts_create import post_api_objects_drafts_create
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+
 from datetime import timedelta
 
 
@@ -22,6 +30,7 @@ class ApiTestCase(TestCase):
         # Client allows us to call the endpoints as if the server was running.
         # Since the server isn't running, this lets us use this for testing.
         self.client = Client()
+        self.factory = APIRequestFactory()
 
         # Sample BCO information for various API calls
         # This will let us insert direclty in the DB or
@@ -36,11 +45,15 @@ class ApiTestCase(TestCase):
             "prefix": 'BCO',
             "schema": 'IEEE',
             "state": 'PUBLISHED',
-            "contents": json.dumps(data[0])
+            "contents": data[0]
         }
         self.sample_bco["object_id"] = 'http://localhost:8000/{}{}'.format(self.sample_bco["object_id_root"], self.sample_bco["object_id_version"])
-
         self.expiration = timezone.now() + timedelta(seconds=600)  # make valid for 10 minutes
+        # self.user = User.objects.create_user(username=self.sample_bco["owner_user"], password="12345")
+
+        self.user = User.objects.get(username=self.sample_bco["owner_user"])
+        # self.user.set_password("12345")
+        # self.user.save()
 
     def create_bco_direct(self):
         """Create BCO directly in DB if needed
@@ -65,23 +78,48 @@ class ApiTestCase(TestCase):
             Creates a prefix.
         """
         # Try to force login as the owner
-        if self.client.force_login(user=User.objects.get(username=self.sample_bco["owner_user"])):
-            response = self.client.post('/api/objects/drafts/create/', {
-                "POST_api_objects_draft_create":
-                    [
-                        {
-                            "prefix": self.sample_bco["prefix"],
-                            "owner_group": self.sample_bco["owner_group"],
-                            "object_id": self.sample_bco["object_id"],
-                            "schema": self.sample_bco["schema"],
-                            "contents": self.sample_bco["contents"],
-                        }
-                    ]
-            })
-            print("Response: ".format(response))
-        else:
-            # Appears login failed
-            print("Failed login")
-            return False
+        token = Token.objects.get(user=self.user)
+
+        bco_request = {
+            "POST_api_objects_draft_create":
+                [
+                    {
+                        "prefix": self.sample_bco["prefix"],
+                        "owner_group": self.sample_bco["owner_group"],
+                        "object_id": self.sample_bco["object_id"],
+                        "schema": self.sample_bco["schema"],
+                        "contents": self.sample_bco["contents"],
+                    }
+                ]
+        }
+        request = self.factory.post(path='/api/objects/drafts/create/', data=bco_request, format='json', HTTP_AUTHORIZATION='Token {}'.format(token))
+        force_authenticate(request, user=self.user)
+
+        response = ApiObjectsDraftsCreate.as_view()(request)
+        print("RESPONSE: {}".format(response))
+        # # if self.client.force_login(user=user):
+        # self.client.force_login(user=self.user)
+        # self.client.login(username=self.sample_bco["owner_user"], password="12345", token=token)
+        # response = self.client.post('/api/objects/drafts/create/', {
+        #     "POST_api_objects_draft_create":
+        #         [
+        #             {
+        #                 "prefix": self.sample_bco["prefix"],
+        #                 "owner_group": self.sample_bco["owner_group"],
+        #                 "object_id": self.sample_bco["object_id"],
+        #                 "schema": self.sample_bco["schema"],
+        #                 "contents": self.sample_bco["contents"],
+        #             }
+        #         ]
+        # })
+        self.assertEqual(response.status_code, 200)
+            # print("Bad response ({}) when trying to test /api/objects/drafts/create/".format(response.status_code))
+            # return False
+
+        # return True
+        # else:
+        #     # Appears login failed
+        #     print("Failed login")
+        #     return False
 
 
