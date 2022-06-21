42abcf585ea04f7070708193a144e004564dfb85
Hadley King <hadley_king@gwu.edu>
2022-06-21 13:40:13 -0400

    Added a 207 response
    207 response means invalid object in payload, 200 means all valid.
    Changes to be committed:
    	modified:   bco_api/api/scripts/method_specific/POST_validate_payload_against_schema.py

--- bco_api/api/scripts/method_specific/POST_validate_payload_against_schema.py
+++ bco_api/api/scripts/method_specific/POST_validate_payload_against_schema.py
@@ -116,6 +116,7 @@ def post_validate_bco(request):
 
     bco_list = request.data["POST_validate_bco"]
     results = {}
+    any_failed = False
     for bco in bco_list:
         identifier = bco["object_id"]
         results[identifier] = {"number_of_errors": 0, "error_detail": []}
@@ -149,5 +150,10 @@ def post_validate_bco(request):
 
         if results[identifier]["number_of_errors"] == 0:
             results[identifier]["error_detail"] = ["BCO Valid"]
+        else:
+            any_failed = True
 
+    if any_failed is True:
+        return Response(status=status.HTTP_207_MULTI_STATUS, data=results)
+    
     return Response(status=status.HTTP_200_OK, data=results)
