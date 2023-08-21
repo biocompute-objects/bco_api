0fff30220bab1e3fa7995313102b92f60a4e00bc
hadleyking <hadley_king@gwu.edu>
2023-08-21 10:47:22 -0400

    Fix response codes and updated tests Changes to be committed: 	modified:   authentication/apis.py 	modified:   tests/fixtures/test_data.json 	modified:   tests/test_views/test_api_auth_remove.py

--- tests/test_views/test_api_auth_remove.py
+++ tests/test_views/test_api_auth_remove.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 """Remove Authentication
-Tests for (#Gives 409 instead of 200(Successfull request)- Requires checking)
-'Bad request' (400),
-'That object already exists for this account' (409)
+Tests for 'Remove authentication is successful.` (200), 'Authentication
+failed.' (403), and 'That object does not exist for this account.' (404)
 """
 
 from django.test import TestCase
@@ -17,40 +16,37 @@ class AuthenticationRemovetestcase(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
-                # Checking if the user 'bco_api_user' already exists
-        try:
-            self.user = User.objects.get(username='bco_api_user')
-        except User.DoesNotExist:
-            self.user = User.objects.create_user(username='bco_api_user')
-
-        # Checking if user already has token, if not then creating one
-        if not Token.objects.filter(user=self.user).exists():
-            self.token = Token.objects.create(user=self.user)
-        else:
-            self.token = Token.objects.get(user=self.user)
 
     def test_success_response(self):
-        """# Successful request with authentication data
-        #Gives a 409 instead of 200
+        """Remove authentication is successful. (200)
         """
-        data = {"iss": "Reeya2","sub": "ReeyaGupta2"}
 
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        response = self.client.post('/api/auth/remove/', data=data, format='json')
+        token = Token.objects.get(user=User.objects.get(username='bco_api_user')).key
         
-        self.assertEqual(response.status_code, 200)
-
-    def test_bad_request_response(self):
-         # Bad request: Missing required fields
-        data = {}  
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        response = self.client.post('/api/auth/remove/', data=data)
-        self.assertEqual(response.status_code, 400)
-
-    def test_object_already_exists_response(self):
-        # Object already exists for this account
         data = {"iss": "Reeya1","sub": "ReeyaGupta1"}
 
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post('/api/auth/remove/', data=data, format='json')
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+
+    def test_bad_authentication(self):
+        """Authentication failed. 403
+        """
+        
+        token = Token.objects.get(user=User.objects.get(username='bco_api_user')).key
+        data = {}  
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
         response = self.client.post('/api/auth/remove/', data=data)
-        self.assertEqual(response.status_code, 409)
+        self.assertEqual(response.status_code, 403)
+
+    def test_object_already_exists_response(self):
+        """That object does not exist for this account. 404
+        """
+        
+        token = Token.objects.get(user=User.objects.get(username='bco_api_user')).key
+        data = {"iss": "Reeya2","sub": "ReeyaGupta2"}
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post('/api/auth/remove/', data=data)
+        self.assertEqual(response.status_code, 404)
