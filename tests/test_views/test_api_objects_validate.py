50c6f1510cbd2561a1421b1838ce47d3a1ab6c05
Reeya Gupta <56750374+Reeya123@users.noreply.github.com>
2023-08-22 13:54:29 -0400

    Create_Prefixes (#222)
    * Create_Prefix
    
    * Update test_api_prefixes_create.py
    
    * Update tests and Swagger Docs for `prefixes_create`
    
    Fix #172
    Changes to be committed:
    	modified:   api/model/prefix.py
    	modified:   api/views.py
    	modified:   tests/test_views/test_api_prefixes_create.py
    
    ---------
    
    Co-authored-by: hadleyking <hadley_king@gwu.edu>
    Co-authored-by: Hadley King <HadleyKing@users.noreply.github.com>

--- tests/test_views/test_api_objects_validate.py
+++ tests/test_views/test_api_objects_validate.py
@@ -1,11 +1,27 @@
 ##Wrong response codes in BCO API documentation. Needs to be worked on!!
 
-from django.test import TestCase, Client
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
 import json
 
 class BcoValidateTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
+        fixtures = ['tests/fixtures/test_data']
+        self.client = APIClient()
+                # Checking if the user 'bco_api_user' already exists
+        try:
+            self.user = User.objects.get(username='bco_api_user')
+        except User.DoesNotExist:
+            self.user = User.objects.create_user(username='bco_api_user')
+
+        # Checking if user already has token, if not then creating one
+        if not Token.objects.filter(user=self.user).exists():
+            self.token = Token.objects.create(user=self.user)
+        else:
+            self.token = Token.objects.get(user=self.user)
         self.url = '/api/objects/validate/'
 
     def test_successful_validation(self):
@@ -13,10 +29,213 @@ class BcoValidateTestCase(TestCase):
         data = {
             "POST_validate_bco": [
                 {
-                    #Some BCO data for validation???
-                }
-            ]
-        }
+                    "object_id": "http://127.0.0.1:8000/BCO_000000/DRAFT",
+				"spec_version": "https://w3id.org/ieee/ieee-2791-schema/2791object.json",
+				"etag": "0275321b6011324035289a5624c635ce5490fbdec588aa5f3bcaf63b85369b4a",
+				"provenance_domain": {
+					"name": "Influenza A reference gene sequences",
+					"version": "1.0",
+					"created": "2021-12-01T15:20:13.614Z",
+					"modified": "2022-06-28T23:10:12.804Z",
+					"review": [
+
+					],
+					"contributors": [
+						{
+							"contribution": [
+								"createdBy",
+								"authoredBy",
+								"curatedBy",
+								"importedBy",
+								"contributedBy"
+							],
+							"name": "Stephanie Singleton",
+							"affiliation": "The George Washington University ",
+							"email": "ssingleton@gwu.edu"
+						},
+						{
+							"contribution": [
+								"createdBy"
+							],
+							"name": "Jonathon Keeney",
+							"affiliation": "The George Washington University ",
+							"email": "keeneyjg@gwu.edu"
+						}
+					],
+					"license": "MIT"
+				},
+				"usability_domain": [
+					"Influenza A (A/Puerto Rico/8/1934 H1N1) reference protein coding sequences.",
+					"Cross reference to genes was retrieved using mappings present in proteins that were retrieved using UniProt proteome ID (UniProt ID: UP000009255; strain A/Puerto Rico/8/1934 H1N1). This set was chosen based on UniProt curation emphasis and community use. The primary use case for this data set is to visualize how protein annotations related to drug resistance mutations, selection pressure and more map to gene sequences. "
+				],
+				"description_domain": {
+					"keywords": [
+						"Influenza A, Complete Genome, FASTA, Genes"
+					],
+					"platform": [
+
+					],
+					"pipeline_steps": [
+						{
+							"step_number": 0,
+							"name": "Download files from UniProt",
+							"description": "Download all files associated with the Influenza A reference genome (influenza A, UP000009255) into the ARGOS Dev server Downloads folder. While logged into the server, execute the following commands: wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Viruses/UP000009255/*. One of the files acquired through this step and necessary for generating a new data set is 'UP000009255_211044_DNA.fasta.gz'. Then execute 'gunzip *.gz' to unzip all the files in the downloads folder. The file name is then changed to 'UP000009255_211044_DNA.fasta' in the downloads folder.",
+							"prerequisite": [
+								{
+									"name": "UniProt reference page ",
+									"uri": {
+										"uri": "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Viruses/UP000009255/",
+										"access_time": "2021-12-01T15:20:13.614Z"
+									}
+								}
+							],
+							"input_list": [
+								{
+									"uri": "ftp://argosdb-vm-dev/data/shared/argosdb/downloads/uniprot/v1.0/influenza_a/UP000009255_211044_DNA.fasta.gz",
+									"filename": "UP000009255_211044_DNA.fasta.gz",
+									"access_time": "2021-12-01T15:20:13.614Z"
+								}
+							],
+							"output_list": [
+								{
+									"uri": "ftp://argosdb-vm-dev/data/shared/argosdb/downloads/uniprot/v1.0/influenza_a/UP000009255_211044_DNA.fasta",
+									"filename": "UP000009255_211044_DNA.fasta",
+									"access_time": "2021-12-01T15:20:13.614Z"
+								}
+							],
+							"version": "1.1"
+						},
+						{
+							"step_number": 0,
+							"name": "Run the recipe created to process this fasta file, review the newly generated dataset, and change the name of the file for clarity",
+							"description": "This step will use a recipe and a python script to generate a new dataset. The recipe tells the python script how and what to construct. This dataset will then be then moved in the 'unreviewed' folder in the dev argosdb server, it will be manually reviewed, and then the name of the file will be changed for clarity and tracking purposes - this is prefered. \\nMake sure you are located in the correct folder to run the script (/software/argosdb/dataset-maker). Use the following command to run the recipe and the python script: ‘python3 make-dataset.py -i recipes/influenza_UP000009255_genome_sequences.json’. Next, go to the ‘unreviewed’ folder to review the newly generated dataset ‘UP000009255_211044_DNA.fasta’. Once reviewed and approved, move the file to the ‘reviewed’ folder. Lastly, once in the ‘reviewed’ folder, change the name of the file to: ‘ influenza_UP000009255_211044_DNA.fasta’",
+							"prerequisite": [
+								{
+									"name": "Dataset-maker python script",
+									"uri": {
+										"uri": "ftp://argosdb-vm-dev/software/argosdb/make-dataset.py",
+										"filename": "make-dataset.py"
+									}
+								},
+								{
+									"name": "Influenza genome FASTA recipe",
+									"uri": {
+										"uri": "ftp://argosdb-vm-dev/data/shared/argosdb/generated/datasets/recipes/Influenza/influenza_UP000009255_genome_sequences.json",
+										"filename": "influenza_UP000009255_genome_sequences.json"
+									}
+								}
+							],
+							"input_list": [
+								{
+									"uri": "ftp://argosdb-vm-dev/data/shared/argosdb/downloads/uniprot/v1.0/influenza_a/UP000009255_211044_DNA.fasta",
+									"filename": "UP000009255_211044_DNA.fasta",
+									"access_time": "2021-12-01T15:20:13.614Z"
+								}
+							],
+							"output_list": [
+								{
+									"uri": "ftp://argosdb-vm-dev/data/shared/argosdb/generated/datasets/reviewed/influenza_UP000009255_211044_DNA.fasta",
+									"filename": "influenza_UP000009255_211044_DNA.fasta",
+									"access_time": "2021-12-01T15:20:13.614Z"
+								}
+							],
+							"version": "1.1"
+						}
+					]
+				},
+				"execution_domain": {
+					"script": [
+						{
+							"uri": {
+								"uri": "ftp://argosdb-vm-dev/software/argosdb/make-dataset.py",
+								"filename": "make-dataset.py"
+							}
+						}
+					],
+					"script_driver": "python3",
+					"software_prerequisites": [
+						{
+							"name": "Python",
+							"version": "3",
+							"uri": {
+								"uri": "https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe",
+								"filename": ""
+							}
+						}
+					],
+					"external_data_endpoints": [
+						{
+							"name": "python-3.10.0",
+							"url": "https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe"
+						}
+					],
+					"environment_variables": {
+					}
+				},
+				"io_domain": {
+					"input_subdomain": [
+						{
+							"uri": {
+								"uri": "http://data.argosdb.org/ln2downloads/uniprot/v1.0/UP000009255_211044_DNA.fasta",
+								"filename": "UP000009255_211044_DNA.fasta"
+							}
+						}
+					],
+					"output_subdomain": [
+						{
+							"mediatype": "text/plain",
+							"uri": {
+								"uri": "http://data.argosdb.org/ln2data/uniprot/v1.0/UP000009255_211044_DNA.fasta",
+								"filename": "UP000009255_211044_DNA.fasta"
+							}
+						}
+					]
+				},
+				"parametric_domain": [
+
+				],
+				"extension_domain": [
+					{
+						"extension_schema": "http://www.w3id.org/biocompute/extension_domain/1.1.0/dataset/dataset_extension.json",
+						"dataset_extension": {
+							"additional_license": {
+								"data_license": "https://creativecommons.org/licenses/by/4.0/",
+								"script_license": "https://www.gnu.org/licenses/gpl-3.0.en.html"
+							},
+							"dataset_categories": [
+								{
+									"category_value": "Influenza A",
+									"category_name": "species"
+								},
+								{
+									"category_value": "nucleotide",
+									"category_name": "molecule"
+								},
+								{
+									"category_value": "Influenza A",
+									"category_name": "tag"
+								},
+								{
+									"category_value": "fasta",
+									"category_name": "file_type"
+								},
+								{
+									"category_value": "reviewed",
+									"category_name": "status"
+								},
+								{
+									"category_value": "internal",
+									"category_name": "scope"
+								}
+							]
+						}
+					}
+				]
+			},
+                
+                ]
+            }
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.post(self.url, data=json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
--- tests/test_views/test_api_prefixes_create.py
+++ tests/test_views/test_api_prefixes_create.py
@@ -1,89 +1,166 @@
-from django.test import TestCase
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient
-from django.contrib.auth.models import User
+#!/usr/bin/env python3
 
-class PrefixCreateTestCase(TestCase):
-    fixtures = ['tests/fixtures/test_data']
+"""Bulk Create Prefixes
+Tests for 'All prefixes were successfully created. 200', 'Some or all prefix
+creations failed. 207', and 'Unauthorized. Authentication credentials were
+not provided. 401'
+
+For the 207 response Each object submitted will have it's own response object
+with it's own status code and message. These are as follows:
+    201: The prefix * was successfully created.
+    400: Bad Request. The expiration date * is not valid.
+    400: Bad Request. The prefix * does not follow the naming rules for a prefix.
+    403: Forbidden. User does not have permission to perform this action.
+    404: Not Found. The user * was not found on the server.
+    409: Conflict. The prefix the requestor is attempting to create already exists.
+ """
+
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import Group
+
+class CreatePrefixeTestCase(APITestCase):
+    fixtures=['tests/fixtures/test_data']
 
     def setUp(self):
-        
-        # Checking if the user 'bco_api_user' already exists
-        try:
-            self.user = User.objects.get(username='bco_api_user')
-        except User.DoesNotExist:
-            self.user = User.objects.create_user(username='bco_api_user')
 
-        # Checking if user already has token, if not then creating one
-        if not Token.objects.filter(user=self.user).exists():
-            self.token = Token.objects.create(user=self.user)
-        else:
-            self.token = Token.objects.get(user=self.user)
+        self.client= APIClient()
 
     def test_create_prefix_success(self):
+        """The prefix was successfully created. 200
+        """
+
+        token = Token.objects.get(user=User.objects.get(username='bco_api_user')).key
         data = {
             "POST_api_prefixes_create": [
                 {
-                    "owner_group": "bco_publisher",
-                    "owner_user": "anon",
+
+                    
+                    "owner_group": "test_drafter",
+                    "owner_user": "bco_api_user",
                     "prefixes": [
                         {
                             "description": "Just a test prefix.",
-                            "expiration_date": "2023-01-01-01-01-01",
                             "prefix": "testR"
-                        },
-                        {
-                            "description": "Just another prefix.",
-                            "expiration_date": "2023-01-01-01-01-01",
-                            "prefix": "othER"
                         }
                     ]
                 }
             ]
         }
 
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
         response = self.client.post('/api/prefixes/create/', data=data, format='json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
 
     def test_create_prefix_bad_request(self):
-        data = {
-            # Incomplete or invalid data
-        }
+        """Tests for 'Some or all prefix creations failed. 207.'
+                201: The prefix * was successfully created.
+                400: Bad Request. The expiration date * is not valid
+                400: Bad Request. The prefix * does not follow the naming rules for a prefix.
+                403: Forbidden. User does not have permission to perform this action.
+                404: Not Found. The user * was not found on the server.
+                409: Conflict. The prefix the requestor is attempting to create already exists.
+        """
 
-        response = self.client.post('/api/prefixes/create/', data=data, format='json')
-        self.assertEqual(response.status_code, 400)
-
-    def test_create_prefix_unauthorized(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token invalid_token')
+        token = Token.objects.get(user=User.objects.get(username='bco_api_user')).key        
         data = {
             "POST_api_prefixes_create": [
                 {
-                    "owner_group": "bco_publisher",
-                    "owner_user": "anon",
+                    "owner_group": "test_drafter",
+                    "owner_user": "bco_api_user",
+                    "prefixes": [
+                        {
+                            "description": "Invalid expiration date.",
+                            "expiration_date": "2023-08-22T09:27:49-0400",
+                            "prefix": "testR"
+                        }
+                    ]
+                },
+                {
+                    "owner_group": "test_drafter",
+                    "owner_user": "bco_api_user",
+                    "prefixes": [
+                        {
+                            "description": "Invalid prefix naming.",
+                            "expiration_date": "null",
+                            "prefix": "invalid-prefix"
+                        }
+                    ]
+                },
+                {
+                    "owner_group": "does_not_exist",
+                    "owner_user": "does_not_exist",
+                    "prefixes": [
+                        {
+                            "description": "Invalid owner.",
+                            "prefix": "testR"
+                        }
+                    ]
+                },
+                {
+                    "owner_group": "test_drafter",
+                    "owner_user": "bco_api_user",
                     "prefixes": [
                         {
                             "description": "Just a test prefix.",
-                            "expiration_date": "2023-01-01-01-01-01",
                             "prefix": "testR"
+                        },
+                        
+                    ]
+                },
+                {
+                    "owner_group": "test_drafter",
+                    "owner_user": "bco_api_user",
+                    "prefixes": [
+                        {
+                            "description": "Just a test prefix.",
+                            "prefix": "other"
                         }
                     ]
                 }
             ]
         }
 
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
         response = self.client.post('/api/prefixes/create/', data=data, format='json')
-        self.assertEqual(response.status_code, 401)
 
-    def test_create_prefix_forbidden(self):
+        # 201: The prefix * was successfully created.
+        self.assertEqual(response.data[3]['status_code'], "201")
+        
+        # 400: Bad Request. The expiration date * is not valid
+        self.assertIn("not valid either because it does not match the required format 'YYYY-MM-DD-HH-MM-SS'", response.data[0]['message'])
+        
+        # 400: Bad Request. The prefix * does not follow the naming rules for a prefix.
+        self.assertIn('does not follow the naming rules for a prefix.', response.data[1]['message'])
+        
+        # TODO =>  403: Forbidden. User does not have permission to perform this action.
+        # This would require testing an instance where the prefix admins was enforced... 
+        
+        # 404: Not Found. The user * was not found on the server.
+        self.assertIn('was not found on the server.', response.data[2]['message'])
+        
+        # 409: Conflict. The prefix the requestor is attempting to create already exists.
+        self.assertIn('has already been created on this server.', response.data[4]['message'])
+    
+        self.assertEqual(response.status_code, 207)
+
+    def test_create_prefix_unauthorized(self):
+        """Unauthorized. Authentication credentials were not provided. 401
+        """
+
         data = {
             "POST_api_prefixes_create": [
                 {
-                    "owner_group": "non_existing_group",
-                    "owner_user": "anon",
+
+                    
+                    "owner_group": "test_drafter",
+                    "owner_user": "bco_api_user",
                     "prefixes": [
                         {
                             "description": "Just a test prefix.",
-                            "expiration_date": "2023-01-01-01-01-01",
                             "prefix": "testR"
                         }
                     ]
@@ -93,27 +170,3 @@ class PrefixCreateTestCase(TestCase):
 
         response = self.client.post('/api/prefixes/create/', data=data, format='json')
         self.assertEqual(response.status_code, 403)
-
-    def test_create_prefix_conflict(self):
-        data = {
-            "POST_api_prefixes_create": [
-                {
-                    "owner_group": "bco_publisher",
-                    "owner_user": "anon",
-                    "prefixes": [
-                        {
-                            "description": "Just a test prefix.",
-                            "expiration_date": "2023-01-01-01-01-01",
-                            "prefix": "testR"
-                        }
-                    ]
-                }
-            ]
-        }
-
-        # Create the prefix first
-        self.client.post('/api/prefixes/create/', data=data, format='json')
-
-        # Try to create the same prefix again
-        response = self.client.post('/api/prefixes/create/', data=data, format='json')
-        self.assertEqual(response.status_code, 409)
