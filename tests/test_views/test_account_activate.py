634c9e1f02ffe18b811f16b9616f1aabd53e2701
hadleyking <hadley_king@gwu.edu>
2024-03-20 14:49:46 -0400

    Cleaning up some things Changes to be committed: 	modified:   biocompute/apis.py 	modified:   biocompute/migrations/0001_initial.py 	modified:   prefix/apis.py 	modified:   prefix/migrations/0001_initial.py 	new file:   tests/test_views/test_account_activate.py 	new file:   tests/test_views/test_account_describe.py 	new file:   tests/test_views/test_account_new.py 	new file:   tests/test_views/test_auth_add.py 	new file:   tests/test_views/test_auth_remove.py 	new file:   tests/test_views/test_auth_reset_token.py 	new file:   tests/test_views/test_objects_drafts_create.py

---
+++ tests/test_views/test_account_activate.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""Test Account Activation 
+Test for '200: Account has been authorized.', '404: Credentials not found.',
+and '403: Requestor's credentials were rejected.'
+"""
+
+import time
+from django.test import TestCase, Client
+
+class ApiAccountsActivateTestCase(TestCase):
+    fixtures = ['tests/fixtures/test_data']
+
+    def setUp(self):
+        self.client = Client()
+    
+    def test_account_activated_success(self):
+        """Test for '201: Account creation request is successful.'
+        """
+            
+        response = self.client.get(
+            '/api/accounts/activate/'\
+            +'test_new_user%40testing.com/sample_temp_identifier'
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_account_activated_forbidden(self):
+        """Test for '403: Requestor's credentials were rejected.'
+        """
+        
+        bad_link = "test_new_user%40testing.com/bad_temp_identifier"
+        response = self.client.get(f'/api/accounts/activate/{bad_link}')
+        self.assertEqual(response.status_code, 403)
+
+    def test_account_activated_not_found(self):
+        """Test for '404: That account, {email}, was not found'
+        """
+        
+        bad_link = "test22%40testing.com/sample_temp_identifier"
+        response = self.client.get(f'/api/accounts/activate/{bad_link}')
+        self.assertEqual(response.status_code, 404)
+
+    def test_account_activated_conflict(self):
+        """Test for '409: CONFLICT: That account, {email},
+        has already been activated.'
+        """
+        
+        bad_link = "tester%40testing.com/sample_temp_identifier"
+        response = self.client.get(f'/api/accounts/activate/{bad_link}')
+        self.assertEqual(response.status_code, 409)
