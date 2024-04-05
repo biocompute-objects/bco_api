de85529957b244a0f193ed49e5e7e2af02f976f9
hadleyking <hadley_king@gwu.edu>
2024-04-04 20:26:35 -0400

    Add DraftsModifyApi
    Changes to be committed:
    	modified:   biocompute/apis.py
    	modified:   biocompute/selectors.py
    	modified:   biocompute/services.py
    	modified:   biocompute/urls.py
    	modified:   prefix/selectors.py
    	deleted:    tests/test_apis/test_biocompute/objects_drafts_create.py
    	deleted:    tests/test_apis/test_biocompute/test_objects_drafts_create.py

--- biocompute/selectors.py
+++ biocompute/selectors.py
@@ -8,8 +8,26 @@ Functions to query the database related to BioCompute Objects
 from django.conf import settings
 from django.contrib.auth. models import User
 from biocompute.models import Bco
-from prefix.selectors import user_can_view
+from prefix.selectors import user_can_view, user_can_modify
 
+def user_can_modify_bco(object_id: str, user:User) -> bool:
+    """Modify BCO
+    """
+
+    try:
+        bco_instance = Bco.objects.get(object_id=object_id)
+    except Bco.DoesNotExist:
+        return None
+    if user in bco_instance.authorized_users.all():
+        return True
+
+    prefix_name = object_id.split("/")[-2].split("_")[0]
+    view_permission = user_can_modify(prefix_name, user)
+    if view_permission is False:
+        return False
+
+    return True
+    
 def retrieve_bco(bco_accession:str, user:User, bco_version:str=None) -> bool:
     """Retrieve BCO
 
@@ -17,11 +35,10 @@ def retrieve_bco(bco_accession:str, user:User, bco_version:str=None) -> bool:
     identified by its accession number and, optionally, its version. It 
     performs several checks:
     
-    1. Checks if the user has general 'view' permissions for the prefix
+    1. Verifies if the BCO exists. If not, returns `None`.
+    2. Checks if the user is explicitly authorized to view this specific BCO.
+    3. Checks if the user has general 'view' permissions for the prefix
         associated with the BCO.
-    2. Verifies if the BCO exists. If not, returns `None`.
-    3. Checks if the user is explicitly authorized to view this specific BCO.
-
     """
 
     hostname = settings.PUBLIC_HOSTNAME
@@ -31,11 +48,6 @@ def retrieve_bco(bco_accession:str, user:User, bco_version:str=None) -> bool:
     else:
         object_id = f"{hostname}/{bco_accession}/{bco_version}"
 
-    prefix_name = bco_accession.split("_")[0]
-    view_permission = user_can_view(prefix_name, user)
-    if view_permission is False:
-        return False
-
     try:
         bco_instance = Bco.objects.get(object_id=object_id)
     except Bco.DoesNotExist:
@@ -44,4 +56,9 @@ def retrieve_bco(bco_accession:str, user:User, bco_version:str=None) -> bool:
     if user in bco_instance.authorized_users.all():
         return bco_instance
 
+    prefix_name = bco_accession.split("_")[0]
+    view_permission = user_can_view(prefix_name, user)
+    if view_permission is False:
+        return False
+    
     return bco_instance
