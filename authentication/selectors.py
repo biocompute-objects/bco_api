d30d378767b8a4606c267a1ba17de78e932fb3a8
hadleyking <hadley_king@gwu.edu>
2024-04-19 12:51:54 -0400

    Update modify_prefix permissions Changes to be committed: 	modified:   authentication/selectors.py 	modified:   config/fixtures/local_data.json 	modified:   prefix/services.py

--- authentication/selectors.py
+++ authentication/selectors.py
@@ -2,10 +2,12 @@
 
 import jwt
 from django.conf import settings
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User
+from django.db.models import Q
 from authentication.models import Authentication, NewUser
 from rest_framework.authtoken.models import Token
 from prefix.selectors import get_user_prefixes
+from prefix.models import Prefix
 from biocompute.selectors import get_authorized_bcos
 
 def get_anon()-> User:
@@ -70,22 +72,18 @@ def get_user_info(user: User) -> dict:
         A dict with the user information.
     """
 
-    token = Token.objects.get(user=user.pk)
-    other_info = {
-        "permissions": {},
-        "account_creation": "",
-    }
-    user_perms = {"prefixes": get_user_prefixes(user), "BCOs": get_authorized_bcos(user)}
-
-    other_info["permissions"] = user_perms
-
-    other_info["account_creation"] = user.date_joined
-
     return {
         "hostname": settings.HOSTNAME,
         "human_readable_hostname": settings.HUMAN_READABLE_HOSTNAME,
         "public_hostname": settings.PUBLIC_HOSTNAME,
-        "token": token.key,
+        "token": Token.objects.get(user=user.pk).key,
         "username": user.username,
-        "other_info": other_info,
+        "permissions": {
+            "owned_prefixes": Prefix.objects.filter(
+                Q(owner=user)
+            ).values_list('prefix', flat=True).distinct(),
+            "permissions": get_user_prefixes(user),
+            "BCOs": get_authorized_bcos(user)
+        },
+        "account_creation": user.date_joined
     }
